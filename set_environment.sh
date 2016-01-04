2d5d4c082ef9934d7d38858ef0101a0c54e1a5be
acefei <acefeilxy@gmail.com>
2016-01-04 16:14:20 +0800

    Update set_environment.sh

--- set_environment.sh
+++ set_environment.sh
@@ -3,13 +3,15 @@ BashProfile=$path/BashProfile
 VimrcWithoutPlugin=$path/VimrcWithoutPlugin
 bashrc=$HOME/.bashrc
 profile=$HOME/.bash_profile
-[ -f $profile ] || touch $profile
+
 for f in $BashProfile/_*
 do
-    echo "source $f >> $profile"
+    echo "source $f into $profile"
     echo "source $f" >> $profile
 done
-
 [ -f $bashrc ] || ln -s $profile $bashrc
 
-cp -f $VimrcWithoutPlugin/_vimrc $HOME/.vimrc
+vimrc=$HOME/.vimrc
+ideavimrc=$HOME/.ideavimrc
+[ -f ] || ln -s $VimrcWithoutPlugin/_vimrc $vimrc
+[ -f ] || ln -s $VimrcWithoutPlugin/_vimrc $ideavimrc
