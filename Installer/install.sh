ed7e9c756a27e6bad7e09a123ac7a3e5267bc95e
fsu <Fei.Su@calix.com>
2017-03-15 13:13:08 +0800

    Add git command verification

--- Installer/install.sh
+++ Installer/install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if which git > /dev/null 2>&1; then
+    echo "please install git first."
+    exit -1
+fi
 cd ~
 git clone https://github.com/acefei/AceProfile.git
 ./AceProfile/Installer/set_environment.sh
