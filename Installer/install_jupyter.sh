c9d0f5e26846850f5784c43e1a356280c86335bf
acefei <acefeilxy@gmail.com>
2017-04-12 10:46:16 +0800

    Update install_jupyter.sh

--- Installer/install_jupyter.sh
+++ Installer/install_jupyter.sh
@@ -10,22 +10,22 @@ mkdir -p $notebook_dir
 
 read -p "Jupyter port [$default_port]:" jupyter_port
 [ "$jupyter_port" == '' ] && jupyter_port=$default_port
-sed -i '/ c.NotebookApp.port / ac.NotebookApp.port = '$jupyter_port'' $jupyter_default_conf
+sed -i '/#c.NotebookApp.port / ac.NotebookApp.port = '$jupyter_port'' $jupyter_default_conf
 
 read -p "Jupyter root path [$notebook_dir]:" jupyter_root
 [ "$jupyter_root" == '' ] && jupyter_root=$notebook_dir
-sed -i '/ c.NotebookApp.notebook_dir / ac.NotebookApp.notebook_dir = u"'$jupyter_root'"' $jupyter_default_conf
+sed -i '/#c.NotebookApp.notebook_dir / ac.NotebookApp.notebook_dir = u"'$jupyter_root'"' $jupyter_default_conf
 
 read -p "Set a password for logining jupyter [$defaut_pwd]:" unauth_pwd
 [ "$unauth_pwd" == '' ] && unauth_pwd=$defaut_pwd
 jupyter_password=$(python -c "from notebook.auth import passwd; print passwd('$unauth_pwd')")
-sed -i '/ c.NotebookApp.password / ac.NotebookApp.password = u"'$jupyter_password'"' $jupyter_default_conf
+sed -i '/#c.NotebookApp.password / ac.NotebookApp.password = u"'$jupyter_password'"' $jupyter_default_conf
 
 # Set all ip addresses on your system are available  
-sed -i '/ c.NotebookApp.ip / ac.NotebookApp.ip = "*"' $jupyter_default_conf
+sed -i '/#c.NotebookApp.ip / ac.NotebookApp.ip = "*"' $jupyter_default_conf
 
 # Jupyter start in silence
-sed -i '/ c.NotebookApp.open_browser / ac.NotebookApp.open_browser = False' $jupyter_default_conf
+sed -i '/#c.NotebookApp.open_browser / ac.NotebookApp.open_browser = False' $jupyter_default_conf
 
 echo
 echo "Jupyter installation complete, please use 'jupyter notebook' to start it."
