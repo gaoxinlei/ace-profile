08abcc632ae8f675d7cf8ae4fc6e8dd785383017
vagrant <vagrant@localhost.localdomain>
2017-10-13 09:16:47 +0000

    refact install vim

--- installer/install.sh
+++ installer/install.sh
@@ -1,34 +1,22 @@
 #!/usr/bin/bash
 
 SUPPORT=(CENTOS UBUNTU)
-RELEASE_FILE=~/.release_info
+
+ACE_PROFILE_ENV=$HOME/.ace_profile_env
+cat > $ACE_PROFILE_ENV <<EOF
+	RELEASE_FILE=$HOME/.release_info
+	INSTALL_PATH=$HOME/.myprofile
+EOF
+
+source $ACE_PROFILE_ENV
 rm -f $RELEASE_FILE
 
 for release in ${SUPPORT[@]}
 do
     echo $release
-done
-exit
-tmp=$(mktemp)
-cd $tmp
-cat /etc/*release > release_info
-if grep -i "CENTOS" release_info > /dev/null; then
-    yum install -y git
-    echo "CENTOS" > $RELEASE_FILE
-elif grep -i "UBUNTU" release_info > /dev/null; then
-    echo "UBUNTU" > $RELEASE_FILE
-fi
-
-
-
-
-
-
-for release in $SUPPORT
-do
     if cat /etc/*release | grep -i $release > /dev/null 2>&1
     then
-        echo $release > ~/.release_info
+        echo $release > $RELEASE_FILE
         break
     fi
 done
@@ -39,15 +27,9 @@ if [ ! -e $RELEASE_FILE ];then
 fi
 
 if ! which git > /dev/null 2>&1; then
-    if which yum > /dev/null 2>&1; then
-        sudo yum install -y git
-    elif which apt-get > /dev/null 2>&1; then
-        sudo apt-get install -y git   
-    else
-        echo "please install git first."
-        exit -1
-    fi
+    echo "please install git first."
+    exit -1
 fi
-cd ~
-git clone https://github.com/acefei/ace-profile.git
-./ace-profile/Installer/set_environment.sh
+
+git clone -b dev https://github.com/acefei/ace-profile.git $INSTALL_PATH
+echo $INSTALL_PATH/installer/
--- installer/precondition.sh
+++ installer/precondition.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
-RELEASE=$(cat ~/.release_info | tr 'a-z' 'A-Z')
+release_info=~/.release_info
+[ -f $release_info ] || echo "CENTOS" > $release_info
+RELEASE=$(cat $release_info | tr 'a-z' 'A-Z')
 
 CWD=$(cd `dirname ${BASH_SOURCE[0]}`; pwd)
-DOWNLOADER='curl -Lo '
+DOWNLOADER='curl -sLO '
 
 # defined known package manager tools 
 YUM='sudo yum install -y '
@@ -11,11 +13,10 @@ YUMG='sudo yum groupinstall -y '
 APTGET='sudo apt-get install -y '
 PIP='sudo pip install'
 
-BashProfile=$CWD/../BashProfile
-Vimrcs=$CWD/../Vimrcs
+bash_profile=$CWD/../bash_profile
+vimrcs=$CWD/../vimrcs
 
 bashrc=$HOME/.bashrc
 profile=$HOME/.bash_profile
 vimrc=$HOME/.vimrc
 ideavimrc=$HOME/.ideavimrc
-
