27feb5b7036cb0c6e3a688ebccb180117639441e
acefei <acefeilxy@gmail.com>
2017-11-06 03:21:58 +0000

    update installer/install_pycharm.sh

--- installer/install_pycharm.sh
+++ installer/install_pycharm.sh
@@ -3,8 +3,9 @@ current_dir=$(cd `dirname ${BASH_SOURCE[0]}`; pwd)
 source $current_dir/precondition.sh
 
 sudo pip install charmy
-sudo charmy install --destination=/opt/
-echo "alias pyc='/opt/PyCharm/pycharm-latest.sh  > /dev/null 2>&1 &'" > ~/.bash_profile
+sudo charmy install --destination=/opt/PyCharm
+echo "alias pyc='/opt/PyCharm/pycharm-latest.sh  > /dev/null 2>&1 &'" >> ~/.bash_profile
 
+echo 
 echo "Please run 'source ~/.bash_profile' to goes into effect."
 echo "Tips: run 'pyc' to start pycharm..."
